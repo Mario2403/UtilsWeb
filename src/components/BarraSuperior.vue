668eef8f595ad6e506f5dfd45b9f473c07fa947d
mario <mario.jurado.soba@alumnos.upm.es>
2020-08-09 18:09:35 +0200

    Inicio de la web

---
+++ src/components/BarraSuperior.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-app-bar>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BarraSuperior"
+};
+</script>
+
