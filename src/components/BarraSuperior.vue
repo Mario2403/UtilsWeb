b20f4d2481c905e20f90b0e7a7eb61788e1a5417
mario <mario.jurado.soba@alumnos.upm.es>
2020-08-09 18:46:51 +0200

    Barra de navegacion superior añadida

--- src/components/BarraSuperior.vue
+++ src/components/BarraSuperior.vue
@@ -1,16 +1,14 @@
 <template>
   <div>
-    <v-app-bar>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title>Page title</v-toolbar-title>
+    <v-app-bar shaped color="#092327" dark>
+      <v-app-bar-nav-icon color="#FFFFFF"></v-app-bar-nav-icon>
+      <v-toolbar-title light>Inicio</v-toolbar-title>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
+<v-spacer/>
+      <v-btn icon href='https://github.com/Mario2403/UtilsWeb'>
+        <v-icon color="#FFFFFF">mdi-github</v-icon>
       </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
     </v-app-bar>
   </div>
 </template>
